7a115250e170fcec52cad77cfae7c22ab1b53763
Diego Oliveira <diegoc.oliveira@gmail.com>
2022-11-16 17:59:42 -0300

    refac: create struct terminal
     * add method input
     * add method ask_for_new_todo
     * add method show_todo

--- src/main.rs
+++ src/main.rs
@@ -11,32 +11,53 @@ impl Todo {
     }
 }
 
-fn main() {
-    println!("😃_>> Olá, gostaria de adicionar um novo TODO? (s/n) ");
-    loop {
-        let answer = input();
-        if answer == "s" {
-            add_todo();
-            println!("😃_>> Gostaria de adicionar outro TODO? (s/n) ");
-        } else if answer == "n" {
-            println!("😃_>> Obrigado por usar o TODO-CLI! 👋");
-            break;
-        } else {
-            println!("🤨_>> Desculpa eu não entendi. Digite 's' se deseja adicionar um novo TODO ou 'n' se deseja sair. ");
+struct Terminal {
+    stdin: Stdin,
+    stdout: Stdout,
+}
+
+impl Terminal {
+    fn new() -> Self {
+        Self {
+            stdin: std::io::stdin(),
+            stdout: std::io::stdout(),
         }
+    }
+
+    fn input(&mut self) -> String {
+        let mut buf = String::new();
+        self.stdin.read_line(&mut buf).unwrap();
+        buf.trim().to_string()
+    }
+
+    fn ask_for_new_todo(&mut self) -> Todo {
+        println!("😃_>> Olá, gostaria de adicionar um novo TODO? (s/n) ");
+        loop {
+            let answer = self.input();
+            if answer == "s" {
+                println!("😃 >> Qual é o TODO?");
+                let message = self.input();
+                let todo = Todo::new(message);
+                return todo;
+            } else if answer == "n" {
+                println!("😃_>> Obrigado por usar o TODO-CLI! 👋");
+                std::process::exit(0);
+            } else {
+                println!("🤨_>> Desculpa eu não entendi. Digite 's' se deseja adicionar um novo TODO ou 'n' se deseja sair. ");
+            }
+        }
+    }
+
+    fn show_todo(&mut self, todo: &Todo) {
+        writeln!(self.stdout, "\n😃_>> O TODO foi adicionado com sucesso! \n").unwrap();
+        writeln!(self.stdout, "📝 - {:?} \n", todo).unwrap();
     }
 }
 
-fn add_todo() {
-    println!("😃 >> Qual é o TODO?");
-    let message = input();
-    let todo = Todo::new(message);
-    println!("\n😃_>> O TODO foi adicionado com sucesso! \n");
-    println!("📝 - {:?} \n", todo);
-}
-
-fn input() -> String {
-    let mut buf = String::new();
-    std::io::stdin().read_line(&mut buf).unwrap();
-    buf.trim().to_string()
+fn main() {
+    let mut terminal = Terminal::new();
+    loop {
+        let todo = terminal.ask_for_new_todo();
+        terminal.show_todo(&todo);
+    }
 }
