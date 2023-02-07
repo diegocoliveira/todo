30bc3b68144c4400efe9689cb5158748bd627bc1
Diego Oliveira <diegoc.oliveira@gmail.com>
2023-02-07 20:30:17 -0300

    refac: terminal::input and terminal::write_line * change trait UserInterface to async trait * use tokio::io for for input and write_line * create fn clean_screen

--- src/cli.rs
+++ src/cli.rs
@@ -29,7 +29,11 @@ impl TodoCli {
         }
     }
     async fn add(&mut self) -> Result<(), CliError> {
-        let item = self.user_interface.add_todo().map_err(CliError::Terminal)?;
+        let item = self
+            .user_interface
+            .add_todo()
+            .await
+            .map_err(CliError::Terminal)?;
         if let Some(todo) = self
             .todo_storage
             .add(item)
@@ -38,14 +42,17 @@ impl TodoCli {
         {
             self.user_interface
                 .show_sucess(&todo, "adicionado com sucesso")
+                .await
                 .map_err(CliError::Terminal)?;
         } else {
             self.user_interface
                 .show_error("Não foi possível adicionar o TODO")
+                .await
                 .map_err(CliError::Terminal)?;
         }
         self.user_interface
             .press_key()
+            .await
             .map_err(CliError::Terminal)?;
         Ok(())
     }
@@ -59,10 +66,12 @@ impl TodoCli {
         {
             self.user_interface
                 .show_sucess(todo, "marcado como feito")
+                .await
                 .map_err(CliError::Terminal)?;
         } else {
             self.user_interface
                 .show_error("Não foi possível marcar o TODO como feito")
+                .await
                 .map_err(CliError::Terminal)?;
         }
         Ok(())
@@ -77,10 +86,12 @@ impl TodoCli {
         {
             self.user_interface
                 .show_sucess(&todo, "deletado com sucesso")
+                .await
                 .map_err(CliError::Terminal)?;
         } else {
             self.user_interface
                 .show_error("Não foi possível deletar o TODO")
+                .await
                 .map_err(CliError::Terminal)?;
         }
 
@@ -96,10 +107,12 @@ impl TodoCli {
         {
             self.user_interface
                 .show_sucess(todo, "atualizado com sucesso!")
+                .await
                 .map_err(CliError::Terminal)?;
         } else {
             self.user_interface
                 .show_error("Não foi possível atualizar o TODO")
+                .await
                 .map_err(CliError::Terminal)?;
         }
         Ok(())
@@ -108,10 +121,12 @@ impl TodoCli {
     async fn edit(&mut self) -> Result<(), CliError> {
         self.user_interface
             .list_todo(self.todo_storage.list().await.map_err(CliError::Storage)?)
+            .await
             .map_err(CliError::Terminal)?;
         if let Some(id) = self
             .user_interface
             .select_todo()
+            .await
             .map_err(CliError::Terminal)?
         {
             if !self
@@ -122,11 +137,13 @@ impl TodoCli {
             {
                 self.user_interface
                     .show_error("Não existe um TODO com esse ID")
+                    .await
                     .map_err(CliError::Terminal)?;
             } else {
                 let action = self
                     .user_interface
                     .ask_for_todo_action(id)
+                    .await
                     .map_err(CliError::Terminal)?;
                 match action {
                     Action::Done(id) => self.done(id).await?,
@@ -138,11 +155,13 @@ impl TodoCli {
         } else {
             self.user_interface
                 .show_error("O ID informado é inválido")
+                .await
                 .map_err(CliError::Terminal)?;
         }
 
         self.user_interface
             .press_key()
+            .await
             .map_err(CliError::Terminal)?;
 
         Ok(())
@@ -151,26 +170,36 @@ impl TodoCli {
     async fn list(&mut self) -> Result<(), CliError> {
         self.user_interface
             .list_todo(self.todo_storage.list().await.map_err(CliError::Storage)?)
+            .await
             .map_err(CliError::Terminal)?;
         self.user_interface
             .press_key()
+            .await
             .map_err(CliError::Terminal)?;
         Ok(())
     }
 
     pub async fn run(&mut self) -> Result<(), CliError> {
-        self.user_interface.welcome().map_err(CliError::Terminal)?;
+        self.user_interface
+            .welcome()
+            .await
+            .map_err(CliError::Terminal)?;
         loop {
             let action = self
                 .user_interface
                 .ask_for_action()
+                .await
                 .map_err(CliError::Terminal)?;
             match action {
                 Action::Add => self.add().await?,
                 Action::List => self.list().await?,
                 Action::Edit => self.edit().await?,
                 Action::Exit => {
-                    return Ok(self.user_interface.exit().map_err(CliError::Terminal)?)
+                    return Ok(self
+                        .user_interface
+                        .exit()
+                        .await
+                        .map_err(CliError::Terminal)?)
                 }
                 _ => (),
             }
--- src/terminal.rs
+++ src/terminal.rs
@@ -1,9 +1,5 @@
-use std::{
-    fmt::Display,
-    io::{self, Write},
-    thread,
-    time::Duration,
-};
+use std::{fmt::Display, thread, time::Duration};
+use tokio::io::{self, AsyncBufReadExt, AsyncWriteExt, BufReader};
 
 use super::todo::Todo;
 
@@ -34,49 +30,66 @@ impl Display for TerminalError {
         }
     }
 }
-
+#[async_trait::async_trait]
 pub trait UserInterface {
-    fn input(&self) -> Result<String, TerminalError>;
-    fn press_key(&mut self) -> Result<(), TerminalError>;
-    fn welcome(&mut self) -> Result<(), TerminalError>;
-    fn exit(&mut self) -> Result<(), TerminalError>;
-    fn ask_for_action(&mut self) -> Result<Action, TerminalError>;
-    fn ask_for_todo_action(&mut self, id: u32) -> Result<Action, TerminalError>;
-    fn add_todo(&mut self) -> Result<String, TerminalError>;
-    fn select_todo(&mut self) -> Result<Option<u32>, TerminalError>;
-    fn list_todo(&mut self, list: Vec<&Todo>) -> Result<(), TerminalError>;
-    fn show_sucess(&mut self, todo: &Todo, msg: &str) -> Result<(), TerminalError>;
-    fn show_error(&mut self, msg: &str) -> Result<(), TerminalError>;
+    async fn input(&mut self) -> Result<String, TerminalError>;
+    async fn press_key(&mut self) -> Result<(), TerminalError>;
+    async fn welcome(&mut self) -> Result<(), TerminalError>;
+    async fn exit(&mut self) -> Result<(), TerminalError>;
+    async fn ask_for_action(&mut self) -> Result<Action, TerminalError>;
+    async fn ask_for_todo_action(&mut self, id: u32) -> Result<Action, TerminalError>;
+    async fn add_todo(&mut self) -> Result<String, TerminalError>;
+    async fn select_todo(&mut self) -> Result<Option<u32>, TerminalError>;
+    async fn list_todo(&mut self, list: Vec<&Todo>) -> Result<(), TerminalError>;
+    async fn show_sucess(&mut self, todo: &Todo, msg: &str) -> Result<(), TerminalError>;
+    async fn show_error(&mut self, msg: &str) -> Result<(), TerminalError>;
 }
 
 pub struct Terminal {
-    version: String,
+    stdin: BufReader<io::Stdin>,
+    stdout: io::Stdout,
     term: Term,
+    version: String,
 }
 
 impl Terminal {
     pub fn new() -> Self {
         Self {
-            //substituição do stdin e stdout por term
+            stdin: BufReader::new(tokio::io::stdin()),
+            stdout: io::stdout(),
             term: Term::stdout(),
             version: "0.8.0".to_string(),
         }
     }
 
-    fn write_line(&mut self, text: &str) -> Result<(), TerminalError> {
-        writeln!(self.term, "{}", text).map_err(TerminalError::Stdout)?;
-
+    async fn write_line(&mut self, text: &str) -> Result<(), TerminalError> {
+        let text = format!("{}\n", text);
+        self.stdout
+            .write(text.as_bytes())
+            .await
+            .map_err(TerminalError::Stdout)?;
         Ok(())
     }
 
-    fn title(&mut self, text: &str) -> Result<(), TerminalError> {
+    async fn clean_screen(&mut self) -> Result<(), TerminalError> {
+        self.stdout
+            .write("\x1Bc\x1B[0K".as_bytes())
+            .await
+            .map_err(TerminalError::Stdout)?;
+        Ok(())
+    }
+
+    async fn title(&mut self, text: &str) -> Result<(), TerminalError> {
+        self.clean_screen().await?;
         self.write_line(&format!(
             "################# {} ################# \n\n",
             style(text).bold().green(),
-        ))?;
+        ))
+        .await?;
         Ok(())
     }
 
+    /*essa fn irá continuando o term */
     fn progress_bar_fake(&mut self) -> Result<(), TerminalError> {
         let mut progess_bar = String::new();
         let mut progess_bar_ok = String::new();
@@ -124,120 +137,149 @@ impl Terminal {
     }
 }
 
+#[async_trait::async_trait]
 impl UserInterface for Terminal {
-    fn input(&self) -> Result<String, TerminalError> {
-        self.term.read_line().map_err(TerminalError::Stdin)
+    async fn input(&mut self) -> Result<String, TerminalError> {
+        //self.term.read_line().map_err(TerminalError::Stdin)
+        let mut buffer = String::new();
+        self.stdin
+            .read_line(&mut buffer)
+            .await
+            .map_err(TerminalError::Stdin)?;
+
+        Ok(buffer.trim().to_string())
     }
 
-    fn press_key(&mut self) -> Result<(), TerminalError> {
-        self.write_line("\n\n Pressione qualquer tecla para continuar ...")?;
+    async fn press_key(&mut self) -> Result<(), TerminalError> {
+        self.write_line("\n\n Pressione qualquer tecla para continuar ...")
+            .await?;
         self.term.read_char().map_err(TerminalError::Stdin)?;
         Ok(())
     }
 
-    fn welcome(&mut self) -> Result<(), TerminalError> {
+    async fn welcome(&mut self) -> Result<(), TerminalError> {
         self.term
             .set_title(&format!("{} - TODO-CLI ", Emoji("📝", "")));
         self.write_line(&format!(
             "\n\n\n{}",
             style("TODO-CLI").bold().underlined().blue(),
-        ))?;
+        ))
+        .await?;
         self.write_line(&format!(
             "\nDesenvolvido por {}",
             style("TerraMagna & AlphaEdtech").red(),
-        ))?;
-        self.write_line(&format!("Versão: {}", style(&self.version).bold().green()))?;
-        self.write_line(&format!("Author: {}\n\n", style("Diego Oliveira").green()))?;
+        ))
+        .await?;
+        self.write_line(&format!("Versão: {}", style(&self.version).bold().green()))
+            .await?;
+        self.write_line(&format!("Author: {}\n\n", style("Diego Oliveira").green()))
+            .await?;
 
         self.progress_bar_fake()?;
-        self.term.clear_screen().map_err(TerminalError::Stdout)?;
+        //self.term.clear_screen().map_err(TerminalError::Stdout)?;
+        self.clean_screen().await?;
 
-        self.write_line(&format!("{}_>> Bem vindo ao TODO-CLI!", Emoji("😃", ":)")))?;
+        self.write_line(&format!("{}_>> Bem vindo ao TODO-CLI!", Emoji("😃", ":)")))
+            .await?;
         thread::sleep(Duration::from_millis(800));
         self.write_line(&format!(
             "{}_>> Aqui você pode adicionar TODOs e ver a lista de TODOs.",
             Emoji("😃", ":)"),
-        ))?;
+        ))
+        .await?;
         thread::sleep(Duration::from_millis(800));
         Ok(())
     }
 
-    fn exit(&mut self) -> Result<(), TerminalError> {
+    async fn exit(&mut self) -> Result<(), TerminalError> {
         self.write_line(&format!(
             "\n{}_>> {} Obrigado por usar o TODO-CLI! ",
             Emoji("😃", ":)"),
             Emoji("👋", "Tchau.")
-        ))?;
+        ))
+        .await?;
         Ok(())
     }
 
-    fn ask_for_action(&mut self) -> Result<Action, TerminalError> {
-        self.write_line("\nAguarde ...")?;
+    async fn ask_for_action(&mut self) -> Result<Action, TerminalError> {
+        self.write_line("\nAguarde ...").await?;
         thread::sleep(Duration::from_millis(2000));
-        self.term.clear_screen().map_err(TerminalError::Stdout)?;
-        self.title("BEM VINDO AO TODO CLI")?;
+        self.title("BEM VINDO AO TODO CLI").await?;
         self.write_line(&format!(
             "{}_>> Olá, como posso te ajudar?",
             Emoji("😃", ":)")
-        ))?;
+        ))
+        .await?;
         self.write_line(&format!(
             "{} >> Digite '{}' para adicionar um novo TODO",
             Emoji("✅", ":)"),
             style("a").bold().green()
-        ))?;
+        ))
+        .await?;
         self.write_line(&format!(
             "{} >> Digite '{}' para listar os TODOs",
             Emoji("🧾", ":)"),
             style("l").bold().green()
-        ))?;
+        ))
+        .await?;
         self.write_line(&format!(
             "{} >> Digite '{}' para selecionar/editar um TODO",
             Emoji("📝", ":)"),
             style("e").bold().green()
-        ))?;
+        ))
+        .await?;
         self.write_line(&format!(
             "{} >> Digite '{}' para sair",
             Emoji("👋", ":)"),
             style("x").bold().red()
-        ))?;
+        ))
+        .await?;
         loop {
             let answer = self.term.read_char().map_err(TerminalError::Stdin)?;
+            self.write_line("").await?; //para quebrar a linha após a resposta
             match answer {
                 'a' => return Ok(Action::Add),
                 'l' => return Ok(Action::List),
                 'e' => return Ok(Action::Edit),
                 'x' => return Ok(Action::Exit),
                 'w' => return Err(TerminalError::Test("AlphaEdtech & TerraMagna".to_string())),
-                _ => self.write_line(&format!(
-                    "{}_>> Desculpa eu não entendi.",
-                    Emoji("🤨", ":/")
-                ))?,
+                _ => {
+                    self.write_line(&format!(
+                        "{}_>> Desculpa eu não entendi.",
+                        Emoji("🤨", ":/")
+                    ))
+                    .await?
+                }
             }
         }
     }
 
-    fn ask_for_todo_action(&mut self, id: u32) -> Result<Action, TerminalError> {
+    async fn ask_for_todo_action(&mut self, id: u32) -> Result<Action, TerminalError> {
         loop {
             self.write_line(&format!(
                 "{} >> Digite '{}' para marcar como feito",
                 Emoji("✅", ":)"),
                 style("f").bold().green()
-            ))?;
+            ))
+            .await?;
             self.write_line(&format!(
                 "{} >> Digite '{}' para editar",
                 Emoji("📝", ":)"),
                 style("e").bold().green()
-            ))?;
+            ))
+            .await?;
             self.write_line(&format!(
                 "{} >> Digite '{}' para deletar",
                 Emoji("🗑 ", ":)"),
                 style("d").bold().green()
-            ))?;
+            ))
+            .await?;
             self.write_line(&format!(
                 "{} >> Digite '{}' para voltar",
                 Emoji("👈", ":)"),
                 style("x").bold().red()
-            ))?;
+            ))
+            .await?;
 
             let answer = self.term.read_char().map_err(TerminalError::Stdin)?;
             match answer {
@@ -246,38 +288,43 @@ impl UserInterface for Terminal {
                     self.write_line(&format!(
                         "{} >> Digite o novo texto do TODO",
                         Emoji("😃", ":)")
-                    ))?;
-                    let text = self.input()?;
+                    ))
+                    .await?;
+                    let text = self.input().await?;
                     return Ok(Action::Update(id, text));
                 }
                 'd' => return Ok(Action::Delete(id)),
                 'x' => return Ok(Action::Exit),
-                _ => self.write_line(&format!(
-                    "{}_>> Desculpa eu não entendi.",
-                    Emoji("🤨", ":/")
-                ))?,
+                _ => {
+                    self.write_line(&format!(
+                        "{}_>> Desculpa eu não entendi.",
+                        Emoji("🤨", ":/")
+                    ))
+                    .await?
+                }
             }
         }
     }
 
-    fn add_todo(&mut self) -> Result<String, TerminalError> {
-        self.term.clear_screen().map_err(TerminalError::Stdout)?;
-        self.title("ADICIONAR TODO")?;
+    async fn add_todo(&mut self) -> Result<String, TerminalError> {
+        self.title("ADICIONAR TODO").await?;
 
         self.write_line(&format!(
             "{} >> Qual é o novo TODO que gostaria de adicionar?",
             Emoji("😃", ":)")
-        ))?;
-        let message = self.input()?;
+        ))
+        .await?;
+        let message = self.input().await?;
         Ok(message)
     }
 
-    fn select_todo(&mut self) -> Result<Option<u32>, TerminalError> {
+    async fn select_todo(&mut self) -> Result<Option<u32>, TerminalError> {
         self.write_line(&format!(
             "\n\n {} >> Informe a chave do Todo que deseja acessar: ",
             Emoji("😃", ":)")
-        ))?;
-        let input = self.input()?;
+        ))
+        .await?;
+        let input = self.input().await?;
         if let Ok(id) = input.parse::<u32>() {
             return Ok(Some(id));
         } else {
@@ -285,15 +332,15 @@ impl UserInterface for Terminal {
         }
     }
 
-    fn list_todo(&mut self, list: Vec<&Todo>) -> Result<(), TerminalError> {
-        self.term.clear_screen().map_err(TerminalError::Stdout)?;
-        self.title("LISTAGEM DOS TODOS")?;
+    async fn list_todo(&mut self, list: Vec<&Todo>) -> Result<(), TerminalError> {
+        self.title("LISTAGEM DOS TODOS").await?;
         if !list.is_empty() {
             self.write_line(&format!(
                 "{}_>> Você tem {} TODOs cadastrados",
                 Emoji("😃", ":)"),
                 style(list.len()).red()
-            ))?;
+            ))
+            .await?;
 
             for todo in list {
                 let color = if todo.done {
@@ -307,31 +354,36 @@ impl UserInterface for Terminal {
                     Emoji("✅", ":)"),
                     color.apply_to(&todo.id),
                     color.apply_to(&todo.message)
-                ))?;
+                ))
+                .await?;
             }
         } else {
             self.write_line(&format!(
                 "{}_>> {}",
                 Emoji("😃", ":)"),
                 style("Você não tem TODOs cadastrados").red()
-            ))?;
+            ))
+            .await?;
         }
         Ok(())
     }
 
-    fn show_sucess(&mut self, todo: &Todo, msg: &str) -> Result<(), TerminalError> {
-        self.write_line(&format!("\n{}_>> O TODO: \n", Emoji("😃", ":)")))?;
+    async fn show_sucess(&mut self, todo: &Todo, msg: &str) -> Result<(), TerminalError> {
+        self.write_line(&format!("\n{}_>> O TODO: \n", Emoji("😃", ":)")))
+            .await?;
 
         self.write_line(&format!(
             "{} - {}! \n",
             style(&todo).italic().magenta(),
             style(msg).green()
-        ))?;
+        ))
+        .await?;
         Ok(())
     }
 
-    fn show_error(&mut self, msg: &str) -> Result<(), TerminalError> {
-        self.write_line(&format!("{}_>> {}", Emoji("😕", ":/"), style(msg).red()))?;
+    async fn show_error(&mut self, msg: &str) -> Result<(), TerminalError> {
+        self.write_line(&format!("{}_>> {}", Emoji("😕", ":/"), style(msg).red()))
+            .await?;
         Ok(())
     }
 }
